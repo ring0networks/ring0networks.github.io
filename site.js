604b558315db48a6512d3b00c47e6025857d1abd
Marcel Moura <marcel.moura@ring-0.io>
2020-12-05 18:42:09 -0300

    Add block and warning pages

---
+++ site.js
@@ -0,0 +1,47 @@
+var img = {
+	1: "https://www.life.com.br/imgs/logolife.png",
+	2: "https://www.fs.com.br/assets/image/logo/head-logo-fs.png"
+};
+
+var bg = {
+	1: "black",
+	2: "white"
+}
+
+var url = {
+	1: "https://life.net.br/",
+	2: "https://www.fs.com.br/"
+}
+
+function querystringValue(name) {
+	var qs = location.search.substring(1),
+		qsPairs = qs.split('&'),
+		i, split,
+		name, value;
+	for (i = 0; i < qsPairs.length; i += 1) {
+		split = qsPairs[i].split('=');
+		key = split[0];
+		value = split[1];
+		if (name === key) {
+			return value;
+		}
+	}
+}
+
+function loadLogo() {
+	var id = querystringValue('id');
+	var custImg = img[id];
+
+	if (custImg) {
+		var header = document.getElementsByTagName('header')[0];
+		var partner = document.getElementById('partner');
+		var partner_url = document.getElementById('partner_url');
+		header.style.display = "block";
+		header.style.backgroundColor = bg[id];
+		partner.style.display = "block";
+		partner.src = custImg;
+		partner_url.href = url[id]
+	}
+}
+
+window.onload = loadLogo();
